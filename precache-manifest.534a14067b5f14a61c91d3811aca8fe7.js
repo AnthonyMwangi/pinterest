6d0605b6355f533aab278b8f1ffd7eb568a4f9a5
Anthony Mwangi <tony.subscryb@gmail.com>
2020-09-03 17:14:28 +0300

    Updates

---
+++ precache-manifest.534a14067b5f14a61c91d3811aca8fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a1b48b6dccafcbe441bba8935401ed",
+    "url": "/pinterest/index.html"
+  },
+  {
+    "revision": "da6956951dc97dd86143",
+    "url": "/pinterest/static/css/main.3c6b5cad.chunk.css"
+  },
+  {
+    "revision": "f0e3fd32aa63317d0f44",
+    "url": "/pinterest/static/js/2.2b1edf8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pinterest/static/js/2.2b1edf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da6956951dc97dd86143",
+    "url": "/pinterest/static/js/main.39b88d5a.chunk.js"
+  },
+  {
+    "revision": "0a4da94b284a23bd5819",
+    "url": "/pinterest/static/js/runtime-main.93ae1bf6.js"
+  },
+  {
+    "revision": "cf000b1f880f531bb6b9c45e16e6e058",
+    "url": "/pinterest/static/media/avi.cf000b1f.jpeg"
+  },
+  {
+    "revision": "1166176f342ec3f5d17e5110ef7d1225",
+    "url": "/pinterest/static/media/logo.1166176f.svg"
+  }
+]);
